f15af2de18a14983ade42ab88151aa5067e081d9
codewithastro <amoriamori3016@gmail.com>
2023-09-08 01:02:20 +0200

    first commit

---
+++ files/standard-integration/public/index.js
@@ -0,0 +1,7 @@
+//Easter Egg Start
+document.getElementById("what").addEventListener("click", function(){
+   alert("Congratulation You Waste Your Time For This Easter Egg :) ")
+})
+//Easter Egg End
+
+//there is no other js code and maybe i will add some in future:)
